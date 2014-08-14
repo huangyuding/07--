415b7a137d0cb2b898d222034b519691eb47f7af
apple <apple@Teacher-316.local>
2014-08-14 14:55:58 +0800

    添加按钮监听方法

--- 01-应用程序管理/HMViewController.m
+++ 01-应用程序管理/HMViewController.m
@@ -38,7 +38,7 @@
     CGFloat marginX = (self.view.bounds.size.width - kColCount * kAppViewW) / (kColCount + 1);
     CGFloat marginY = 10;
 
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < self.appList.count; i++) {
         // 行
         // 0, 1, 2 => 0
         // 3, 4, 5 => 1
@@ -109,7 +109,67 @@
         button.titleLabel.font = [UIFont systemFontOfSize:12.0];
         
         [appView addSubview:button];
+        
+        // 给按钮添加监听方法
+        button.tag = i;
+        
+        [button addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
     }
+}
+
+/** 按钮监听方法 */
+- (void)click:(UIButton *)button
+{
+    NSLog(@"%s %d", __func__, button.tag);
+    
+    // 取出appInfo
+    HMAppInfo *appInfo = self.appList[button.tag];
+    
+    // 添加一个UILabel到界面上
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(80, 400, 160, 40)];
+    // 数值是0表示黑色，1表示纯白
+    // alpha表示透明度
+    label.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.2];
+    
+    label.text = appInfo.name;
+    label.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:label];
+    // 动画效果
+    // 收尾式动画，修改对象的属性，frame,bounds,alpha
+    // 初始透明度，完全透明
+    label.alpha = 0.0;
+    
+    // 禁用按钮
+    button.enabled = NO;
+    
+    // 动画结束之后删除
+    // ^ 表示是block，块代码，是一个预先准备好的代码块，可以当做参数传递，在需要的时候执行！
+    // 块代码在OC中，使用的非常普遍！
+    [UIView animateWithDuration:1.0f animations:^{
+        NSLog(@"动画开始");
+        // 要修改的动画属性
+        label.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1.0 animations:^{
+            label.alpha = 0.0;
+        } completion:^(BOOL finished) {
+            // 动画完成后，所做的操作
+            NSLog(@"动画完成");
+            
+//            button.enabled = NO;
+            
+            [label removeFromSuperview];
+        }];
+    }];
+    
+    NSLog(@"-------");
+    
+    // 收尾式动画，不容易监听动画完成时间，而且不容易实现动画嵌套
+//    [UIView beginAnimations:nil context:nil];
+//    [UIView setAnimationDuration:1.0f];
+//    label.alpha = 1.0;
+//    [UIView commitAnimations];
 }
 
 @end
