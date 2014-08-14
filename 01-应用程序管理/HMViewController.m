9427696d7e225765fde4d89304a9547ed09e5175
apple <apple@Teacher-316.local>
2014-08-14 10:34:08 +0800

    九宫格内部细节实现

--- 01-应用程序管理/HMViewController.m
+++ 01-应用程序管理/HMViewController.m
@@ -9,10 +9,19 @@
 #import "HMViewController.h"
 
 @interface HMViewController ()
-
+/** 应用程序列表 */
+@property (nonatomic, strong) NSArray *appList;
 @end
 
 @implementation HMViewController
+
+- (NSArray *)appList
+{
+    if (_appList == nil) {
+        _appList = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"app.plist" ofType:nil]];
+    }
+    return _appList;
+}
 
 - (void)viewDidLoad
 {
@@ -44,30 +53,56 @@
         CGFloat y = kStartY + marginY + row * (marginY + kAppViewH);
         
         UIView *appView = [[UIView alloc] initWithFrame:CGRectMake(x, y, kAppViewW, kAppViewH)];
-        appView.backgroundColor = [UIColor redColor];
+//        appView.backgroundColor = [UIColor redColor];
         [self.view addSubview:appView];
+        
+        // 实现视图内部细节
+        NSDictionary *dict = self.appList[i];
+        
+        // 1> UIImageView
+        UIImageView *icon = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kAppViewW, 50)];
+//        icon.backgroundColor = [UIColor greenColor];
+        
+        // 设置图像
+        icon.image = [UIImage imageNamed:dict[@"icon"]];
+        // 设置图像填充模式，等比例显示(CTRL+6)
+        icon.contentMode = UIViewContentModeScaleAspectFit;
+        
+        [appView addSubview:icon];
+        
+        // 2> UILabel -> 应用程序名称
+        // CGRectGetMaxY(frame) = frame.origin.y + frame.size.height
+        UILabel *lable = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(icon.frame), kAppViewW, 20)];
+//        lable.backgroundColor = [UIColor blueColor];
+        
+        // 设置应用程序名称
+        lable.text = dict[@"name"];
+        // 设置字体
+        lable.font = [UIFont systemFontOfSize:13.0];
+        lable.textAlignment = NSTextAlignmentCenter;
+        
+        [appView addSubview:lable];
+        
+        // 3> UIButton -> 下载按钮
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(lable.frame), kAppViewW, 20)];
+        button.backgroundColor = [UIColor yellowColor];
+        
+        // 背景图片
+        [button setBackgroundImage:[UIImage imageNamed:@"buttongreen"] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:@"buttongreen_highlighted"] forState:UIControlStateHighlighted];
+        
+        // 按钮都是有状态的，不同状态可以对应不同的标题
+        [button setTitle:@"下载" forState:UIControlStateNormal];
+        // *** 一定不要使用以下方法，修改按钮标题
+//        button.titleLabel.text = @"aaa";
+        
+        // 修改字体（titleLabel是只读的）
+        // readonly表示不允许修改titleLabel的指针，但是可以修改label的字体
+        // 提示：按钮的字体是不区分状态的！
+        button.titleLabel.font = [UIFont systemFontOfSize:12.0];
+        
+        [appView addSubview:button];
     }
-    
-//    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(120, 20, 80, 90)];
-//    view2.backgroundColor = [UIColor redColor];
-//    [self.view addSubview:view2];
-//    
-//    UIView *view3 = [[UIView alloc] initWithFrame:CGRectMake(220, 20, 80, 90)];
-//    view3.backgroundColor = [UIColor redColor];
-//    [self.view addSubview:view3];
-//    
-//    UIView *view4 = [[UIView alloc] initWithFrame:CGRectMake(20, 120, 80, 90)];
-//    view4.backgroundColor = [UIColor redColor];
-//    [self.view addSubview:view4];
-//    
-//    UIView *view5 = [[UIView alloc] initWithFrame:CGRectMake(120, 120, 80, 90)];
-//    view5.backgroundColor = [UIColor redColor];
-//    [self.view addSubview:view5];
-//    
-//    UIView *view6 = [[UIView alloc] initWithFrame:CGRectMake(220, 120, 80, 90)];
-//    view6.backgroundColor = [UIColor redColor];
-//    [self.view addSubview:view6];
 }
-
 
 @end
